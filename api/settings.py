50cf85fd1da82969f0784f84e74efee83d91072a
Odewale Richard <odewalerichard@gmail.com>
2025-10-16 19:08:56 +0100

    Final commit

--- api/settings.py
+++ api/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY =str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://feelnigeriabackend-343v.onrender.com","http://feelnigeriabackend-343v.onrender.com","feelnigeriabackend-343v.onrender.com"]
 
 
 # Application definition

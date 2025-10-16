02355274c1cabe325aea24b6cf8cd82f4c4664a0
Odewale Richard <odewalerichard@gmail.com>
2025-10-16 18:26:03 +0100

    Fourth Deployment

--- api/settings.py
+++ api/settings.py
@@ -17,7 +17,12 @@ SECRET_KEY =str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['feelnigeriabackend-343v.onrender.com','http://feelnigeriabackend-343v.onrender.com','https://feelnigeriabackend-343v.onrender.com']
+ALLOWED_HOSTS = [
+    'feelnigeriabackend-343v.onrender.com',
+    'www.feelnigeriabackend-343v.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition

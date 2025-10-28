2498fe775d4f045a26b9db740e4c98199397de05
Odewale Richard <odewalerichard@gmail.com>
2025-10-28 21:09:24 +0100

    BVN endpoint

--- applications/urls.py
+++ applications/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("step2/<str:id>/", Step2View.as_view(), name="step2"),
     path("step3/<str:id>/", Step3View.as_view(), name="step3"),
     path("step4/<str:id>/", Step4View.as_view(), name="step4"),
+    path("bvn/<str:id>/", BVNView.as_view(), name="step4"),
     path("review/<str:id>/", ReviewView.as_view(), name="review"),
     path("submit/<str:id>/", SubmissionView.as_view(), name="submit"),
 ]
